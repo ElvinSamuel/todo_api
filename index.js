f431217ff3bc2aaba728c14d148851db4f048e7a
Elvin Samuel <elvinsamuel1@gmail.com>
2018-08-06 14:14:27 +0000

    Added a default port (process.env.PORT || 3000) in case I run this code locally

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var express = require('express'),
-    app     = express();
+    app     = express(),
+    /* Adding in the Port */
+    port    = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
     res.send("Hi!");
 });
 
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(port, function(){
     console.log(`To-Do List Up & Running On Port ${process.env.PORT}`);
 });
